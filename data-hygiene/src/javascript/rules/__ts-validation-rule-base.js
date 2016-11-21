47376ad7065db11a9cc482a4472f9ca0452b475e
Kristy Corkan <kcorkan@gmail.com>
2016-11-21 11:48:39 -0700

    updated feature field on user story

--- data-hygiene/src/javascript/rules/__ts-validation-rule-base.js
+++ data-hygiene/src/javascript/rules/__ts-validation-rule-base.js
@@ -43,7 +43,7 @@ Ext.define('CA.techservices.validation.BaseRule',{
         });
     },
     _getFeatureName: function(){
-        return "Feature";
+       // return "Feature";
         return this.portfolioItemTypes[0].TypePath.replace('PortfolioItem/','');
     },
     getUserFriendlyRuleLabel: function() {
