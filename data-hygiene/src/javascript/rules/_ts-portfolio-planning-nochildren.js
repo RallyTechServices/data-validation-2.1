a4130069d8ae459766d42303be27a050938fe4b1
Kristy Corkan <kcorkan@gmail.com>
2016-12-28 22:05:59 -0700

    added files

---
+++ data-hygiene/src/javascript/rules/_ts-portfolio-planning-nochildren.js
@@ -0,0 +1,54 @@
+Ext.define('CA.techservices.validation.PortfolioStateNoStories',{
+    extend: 'CA.techservices.validation.BaseRule',
+    alias:  'widget.tsportfolio_statenostories',
+
+
+    config: {
+        /*
+         * [{Rally.wsapi.data.Model}] portfolioItemTypes the list of PIs available
+         * we're going to use the first level ones (different workspaces name their portfolio item levels differently)
+         */
+        portfolioItemTypes:[],
+        portfolioItemStates: [],
+        targetPortfolioLevel: 0,
+        triggerState: "Planning",
+
+        label: '{0} in "{1}" State or beyond with no child stories',
+        description: '{0} in "{1}" State or beyond with no child stories'
+    },
+    getModel:function(){
+        return this.portfolioItemTypes[this.targetPortfolioLevel].TypePath;
+    },
+    getLabel: function(){
+        return Ext.String.format(this.label, this.portfolioItemTypes[this.targetPortfolioLevel].Name, this.triggerState);
+    },
+    getFilters: function(){
+        var filters = [],
+            piName = this.portfolioItemTypes[this.targetPortfolioLevel].TypePath,
+            states = this.portfolioItemStates[piName];
+        Ext.Array.each(states, function(state){
+            if (state === this.triggerState || filters.length > 0){
+                filters.push({
+                    property: 'State.Name',
+                    value: state
+                });
+            }
+        }, this);
+
+        if (filters.length > 1){
+            filters = Rally.data.wsapi.Filter.or(filters);
+            filters = filters.and({
+                property: 'LeafStoryCount',
+                value: 0
+            });
+        } else {
+            filters.push({
+                property: 'LeafStoryCount',
+                value: 0
+            });
+            filters = Rally.data.wsapi.Filter.and(filters);
+        }
+
+        return filters;
+    }
+});
